c0b58bfc8f6957ceedc891e9092875e759a15a56
Jacobwj04 <32894@ma-web.nl>
2021-10-15 11:33:22 +0200

    vesion1.0

---
+++ BeroepOpdracht/Eindopdracht/BOopdracht.py
@@ -0,0 +1,27 @@
+print("Ik word wakker door geschreeuuw mensen op staart zeiden vluchten er is oorlog. Ik hab gelukt ik was verkleed. Ik liet alls achter en begon te rennen.")
+print("A: Ga ik door met rennen")
+print("B: Ik stop om te rusten ")
+answer = input()
+if answer == "A" or answer == "a":
+    print("Ik ga uit bed en ik ga eten")
+
+    print("")
+    print("")
+    print("")
+    
+    answer = input()
+    if answer == "A":
+        print("")
+        print("A: .")
+        print("B: ")
+        
+    elif answer == "B":
+        print("")
+        print("A: ")
+        print("B: ")
+       
+    
+elif answer == "B":
+    print("")
+    print("")
+   
